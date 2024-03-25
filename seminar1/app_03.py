a7fafb366297ab164b8f5f674a18307bd1ce52c0
Mikhail <mikhail-mikheev-7@yandex.ru>
2024-03-25 23:50:01 +0300

    add app_03.py

---
+++ seminar1/app_03.py
@@ -0,0 +1,23 @@
+"""Задание №3
+Написать функцию, которая будет принимать на вход два
+числа и выводить на экран их сумму.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello!'
+
+@app.route('/<int:a>/<int:b>')
+def sum(a,b):
+    return str(a+b)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
